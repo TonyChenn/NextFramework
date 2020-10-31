b2d9deb379f55ee06a645f8dcb6070c8cf2528e1
TonyChenn <852454151@qq.com>
2020-10-31 17:21:55 +0800

    更新文件操作工具

--- Assets/NextFramework/Util/FileHelper.cs
+++ Assets/NextFramework/Util/FileHelper.cs
@@ -1,23 +1,50 @@
 ﻿using System.IO;
 
-namespace NextFramework.Util
+public class FileHelper
 {
-    public class FileHelper
+    public static string ReadFile(string path)
     {
-        public static string ReadFile(string path)
+        string str = "";
+        if (File.Exists(path))
+            str = File.ReadAllText(path);
+        else
+            UnityEngine.Debug.LogError("File is not Exists：" + path);
+        return str;
+    }
+    public static void WriteFile(string path, string content)
+    {
+        if (!File.Exists(path))
+            File.Create(path).Dispose();
+        File.WriteAllText(path, content);
+    }
+
+    public static void WriteUITypeFile(string filePath, string content = "")
+    {
+        string fileContent = NextFramework.TemplateString.Str_UIType.Replace("{ENUM}", content);
+
+        WriteFile(filePath, fileContent);
+    }
+
+    public static string UIPanelJson
+    {
+        get
         {
-            string str = "";
-            if (File.Exists(path))
-                str = File.ReadAllText(path);
+            if (File.Exists(PathConfig.UIPanelsJsonPath))
+                return ReadFile(PathConfig.UIPanelsJsonPath);
             else
-                UnityEngine.Debug.LogError("File is not Exists：" + path);
-            return str;
+                return "[]";
         }
-        public static void WriteFile(string path, string content)
+        set
         {
-            if (!File.Exists(path))
-                File.Create(path).Dispose();
-            File.WriteAllText(path, content);
+            WriteFile(PathConfig.UIPanelsJsonPath, value);
         }
+    }
+}
+public class FolderHelper
+{
+    public static void CreateFolder(string folderPath)
+    {
+        if (!Directory.Exists(folderPath))
+            Directory.CreateDirectory(folderPath);
     }
 }
