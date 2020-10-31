f3e005b6deb37b2d122b351ff6393233920d999b
TonyChenn <852454151@qq.com>
2020-10-31 17:23:22 +0800

    xLua模块

---
+++ Assets/NextFramework/xLua/LuaCompoment.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+[System.Serializable]
+public class Injection
+{
+    public string name;
+    public GameObject value;
+}
+
+[LuaCallCSharp]
+public class LuaCompoment : MonoBehaviour
+{
+    public string luaScript;
+    public Injection[] injections;
+
+    internal static float lastGCTime = 0;
+    internal const float GCInterval = 1;//1 second 
+
+    private Action luaStart;
+    private Action luaUpdate;
+    private Action luaOnDestroy;
+
+    private LuaTable scriptEnv;
+
+    void Awake()
+    {
+        if(string.IsNullOrEmpty(luaScript))
+        {
+            Debug.LogError("Lua script Name is not set,please check!!!");
+            return;
+        }
+
+        scriptEnv = XLuaManger.Singlton.LuaEnv.NewTable();
+
+        // 为每个脚本设置一个独立的环境，可一定程度上防止脚本间全局变量、函数冲突
+        LuaTable meta = XLuaManger.Singlton.LuaEnv.NewTable();
+        meta.Set("__index", XLuaManger.Singlton.LuaEnv.Global);
+        scriptEnv.SetMetaTable(meta);
+        meta.Dispose();
+
+        scriptEnv.Set("self", this);
+        foreach (var injection in injections)
+            scriptEnv.Set(injection.name, injection.value);
+
+        XLuaManger.Singlton.LuaEnv.DoString(string.Format("require '{0}'", luaScript));
+
+        Action luaAwake = scriptEnv.Get<Action>("Awake");
+        scriptEnv.Get("Start", out luaStart);
+        scriptEnv.Get("Update", out luaUpdate);
+        scriptEnv.Get("OnDestroy", out luaOnDestroy);
+
+        if (luaAwake != null)
+            luaAwake();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        if (luaStart != null)
+            luaStart();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (luaUpdate != null)
+            luaUpdate();
+
+        if (Time.time - lastGCTime > GCInterval)
+        {
+            XLuaManger.Singlton.LuaEnv.Tick();
+            lastGCTime = Time.time;
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (luaOnDestroy != null)
+            luaOnDestroy();
+
+        luaOnDestroy = null;
+        luaUpdate = null;
+        luaStart = null;
+        scriptEnv.Dispose();
+        injections = null;
+    }
+}
---
+++ Assets/NextFramework/xLua/XLuaManger.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using NextFramework;
+using XLua;
+using System.IO;
+
+public class XLuaManger : IDisposable
+{
+    LuaEnv mLuaEnv;
+    public LuaEnv LuaEnv { get { return mLuaEnv; } }
+    
+    static XLuaManger _instance = null;
+    public static XLuaManger Singlton { get { return _instance ?? new XLuaManger(); } }
+
+    public XLuaManger()
+    {
+        mLuaEnv = new LuaEnv();
+        LuaEnv.CustomLoader loader;
+#if UNITY_EDITOR
+        if (GameConfig.UseLocalAsset)
+            loader = OriginLoader;
+        else
+            loader = BundleLoader;
+#else
+        loader = BundleLoader;
+#endif
+        mLuaEnv.AddLoader(loader);
+    }
+    ~XLuaManger()
+    {
+        Dispose(false);
+    }
+
+    private byte[] OriginLoader(ref string fileName)
+    {
+        string luafile = PathConfig.XLuaScriptFolder + "/" + fileName + ".lua";
+        if(!File.Exists(luafile))
+        {
+            Debug.LogError("cant find file:" + fileName + " in path:" + luafile);
+            return null;
+        }
+        return File.ReadAllBytes(luafile);
+    }
+    private byte[] BundleLoader(ref string fileName)
+    {
+        return null;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+    void Dispose(bool dispose)
+    {
+        if (dispose)
+        {
+            if (mLuaEnv != null)
+            {
+                mLuaEnv.GC();
+                mLuaEnv.Dispose();
+            }
+        }
+        mLuaEnv = null;
+    }
+}
