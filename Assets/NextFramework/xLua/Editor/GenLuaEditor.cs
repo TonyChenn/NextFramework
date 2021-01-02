69d8a37285587641953df6438ed7ae73a9ac7d33
TonyChenn <852454151@qq.com>
2021-01-02 18:45:06 +0800

    XLua生成代码

---
+++ Assets/NextFramework/xLua/Editor/GenLuaEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class GenLua
+{
+    [MenuItem("Assets/生成Lua")]
+    public static void GenLuaFile()
+    {
+        var selectObj = Selection.activeGameObject;
+        if (selectObj != null && selectObj.GetType() == typeof(GameObject))
+        {
+            string path = AssetDatabase.GetAssetPath(selectObj);
+        }
+    }
+}
