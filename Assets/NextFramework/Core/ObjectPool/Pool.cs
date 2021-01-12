46c94fe27f846fdc7355e2c297a2513724c066b9
TonyChenn <852454151@qq.com>
2021-01-12 21:29:28 +0800

    对象池

--- Assets/NextFramework/Core/ObjectPool/Pool.cs
+++ Assets/NextFramework/Core/ObjectPool/Pool.cs
@@ -10,20 +10,19 @@ namespace NextFramework
         protected int mMaxCount = 10;
         protected IObjectFactory<T> factory;
 
+        public int HasCount { get { return dataStack.Count; } }
+        
         /// <summary>
         /// 获取
         /// </summary>
         public virtual T Alloc()
         {
-            return Count == 0 ? factory.Create() : dataStack.Pop();
+            return HasCount == 0 ? factory.Create() : dataStack.Pop();
         }
-
         /// <summary>
         /// 回收
         /// </summary>
         public abstract bool Recycle(T obj);
-
-        public int Count { get { return dataStack.Count; } }
     }
 }
 
