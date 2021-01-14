bcab46c4d894e42761a6881c8c2490aa9945ea37
TonyChenn <852454151@qq.com>
2021-01-14 17:45:44 +0800

    TextStyle

---
+++ Assets/NextFramework/Extention/UGUIEx/Text/TextStyle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace NextFramework
+{
+    [CreateAssetMenu(fileName = "TextStyle", menuName ="New TextStyle", order =1)]
+    public class TextStyle : ScriptableObject
+    {
+        [SerializeField] Font mFont;
+        [SerializeField] FontStyle mFontStyle;
+        [Range(1, 300)]
+        [SerializeField] int mFontSize;
+
+
+        public void Apply(UnityEngine.UI.Text text)
+        {
+            if (text == null) return;
+
+            text.font = mFont;
+            text.fontStyle = mFontStyle;
+            text.fontSize = mFontSize;
+        }
+    }
+}
+
