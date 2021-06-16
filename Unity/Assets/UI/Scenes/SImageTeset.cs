a446dd73bfa17ca70dc8891ecb632ba2a8db959f
tonychenn <852454151@qq.com>
2021-06-16 18:03:52 +0800

    temp upload

---
+++ Unity/Assets/UI/Scenes/SImageTeset.cs
@@ -0,0 +1,37 @@
+﻿using Common;
+using NextFramework.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class SImageTeset : MonoBehaviour
+{
+    [SerializeField] private SImage Image;
+
+    SpriteAtlas CommonAtlas;
+    void Start()
+    {
+        //string path = $"{Application.streamingAssetsPath}/ui/uiatlas/common.u3d";
+        //AtlasAssetItem atlas = new AtlasAssetItem("common");
+        //atlas.Load(() =>
+        //{
+        //    CommonAtlas = atlas.AtlasObject;
+        //    Image.SetSprite(CommonAtlas, "icon_1");
+        //});
+        //CommonAtlas = LoadAtlas(path);
+
+        
+    }
+
+    SpriteAtlas LoadAtlas(string path)
+    {
+        AssetBundle bundle = AssetBundle.LoadFromFile(path);
+        if (bundle == null)
+        {
+            Debug.LogError("null");
+            return null;
+        }
+        return bundle.LoadAsset<SpriteAtlas>("common");
+    }
+}
---
+++ Unity/Assets/UI/Scenes/Test_001.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.UI;
+
+public class Test_001 : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        SpriteAtlasManager.atlasRequested += RequestAtlas;
+    }
+    private void OnDisable()
+    {
+        SpriteAtlasManager.atlasRequested -= RequestAtlas;
+    }
+
+    /// <summary>
+    /// 加载图集
+    /// </summary>
+    private void RequestAtlas(string tag, Action<SpriteAtlas> callback)
+    {
+        AssetBundle ab = AssetBundle.LoadFromFile($"{Application.streamingAssetsPath}/ui/uiatlas/{tag}.u3d");
+        SpriteAtlas atlas = ab.LoadAsset<SpriteAtlas>(tag);
+        callback(atlas);
+    }
+}
